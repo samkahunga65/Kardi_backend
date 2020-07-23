498bce99d5a1e1478301c1ebed10849cf1ebd907
ohongs <samkahunga65@gmail.com>
2020-07-23 17:20:34 +0300

    ad

--- pokerServer/settings.py
+++ pokerServer/settings.py
@@ -136,3 +136,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+AUTH_USER_MODEL = 'accounts.User'
