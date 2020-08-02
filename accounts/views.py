2df881f0fa49e03b994b3b24ada840b6051a97a0
ohongs <samkahunga65@gmail.com>
2020-08-02 13:11:53 +0300

    gg

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import PlayerSerializer, UserSerializer
@@ -34,6 +35,7 @@ def user(request, famid):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def userAndPlayer(request, id):
     user = User.objects.get(pk=id)
     player = Player.objects.get(owner=id)
